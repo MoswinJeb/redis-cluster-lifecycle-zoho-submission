fac127574b2aac0fcdfb404db0220a541f20db28
MoswinJeb <moswinjebaraj@gmail.com>
2026-05-25 16:55:14 +0530

    Finalize Redis cluster lifecycle automation workflow

--- infra/startup.sh
+++ infra/startup.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-nohup redis-server \
-  --port 6379 \
-  --cluster-enabled yes \
-  --cluster-config-file /var/lib/redis/nodes.conf \
-  --cluster-node-timeout 5000 \
-  --cluster-announce-ip $(hostname -i) \
-  --cluster-announce-port 6379 \
-  --cluster-announce-bus-port 16379 \
-  --appendonly yes \
-  --protected-mode no \
-  --bind 0.0.0.0 \
-  > /tmp/redis.log 2>&1 &
-
 exec /usr/sbin/sshd -D
